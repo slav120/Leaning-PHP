2b0719016e3e2c0e357d85434af7dd66e34ae0c1
Slav <slalev57@me.com>
2020-01-24 11:43:21 -0500

    the echo command

---
+++ echo.php
@@ -0,0 +1,20 @@
+<?php
+// Write your code below:
+echo "Hello, World!";
+
+// Returns    Hello, World! 
+  
+// Write your code below:
+
+echo "She said \"hi\" to the dog.";
+
+echo "1. Go to gym"; 
+echo "\n2. Cook dinner"; 
+echo "\n3. Learn to have \"fun\""; 
+
+// Returns that on different lines 
+
+
+echo "one" . "two"; // Prints: onetwo
+echo "one " . "two"; // Prints: one two
+
