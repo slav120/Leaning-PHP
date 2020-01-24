c32ed99b94866adda51c0c001836b59159e6c23a
Slav <slalev57@me.com>
2020-01-24 12:00:06 -0500

    variables and assigning them

---
+++ variables.php
@@ -0,0 +1,25 @@
+<?php
+
+<?php
+// Write your code below:
+
+// Variables alaways start with $ always use snake case  
+$my_name = "Slav Lalev";  
+$biography = "\n Hello im from bla bla";
+$favorite_food = "\n" . "tur" . "duck" . "en";
+
+
+$favorite_food = "Red curry with eggplant, green beans, and peanuts";
+echo $favorite_food; 
+// Prints: Red curry with eggplant, green beans, and peanuts
+
+$dog_name = "Tadpole";
+echo "My dog is named " . $dog_name; 
+// Prints: My dog is named Tadpole
+
+$dog_name = "Tadpole";
+$favorite_food = "salmon";
+$color = "brown";
+
+echo "I have a " . $color . " dog named " . $dog_name . " and her favorite food is " . $favorite_food . ".";
+// Prints: I have a brown dog named Tadpole and her favorite food is salmon.
