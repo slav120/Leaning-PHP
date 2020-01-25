95a04e4e471f7f3de9d64762fef34d4962ad8339
Slav <slalev57@me.com>
2020-01-25 17:30:02 -0500

    learning how to reference assign variables

---
+++ referenceAssign.php
@@ -0,0 +1,15 @@
+<?php
+/* Imagine a lot of code here */  
+  $very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+
+$order =& $very_bad_unclear_name; 
+$order .= ", you want some fries with that ?";
+  
+  // Don't change the line below
+  echo "\nYour order is: $very_bad_unclear_name.";
+
+// PRINTS 
+
+// Your order is: 15 chicken wings, you want some fries with that ?.
