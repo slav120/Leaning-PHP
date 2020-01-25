8b08063e3215cc72bc6a07650706690a5c09e06b
Slav <slalev57@me.com>
2020-01-25 18:08:42 -0500

    multiplication and division

--- numbers.php
+++ numbers.php
@@ -26,3 +26,18 @@ echo $age; // Prints: 84
 $last_month = 1187.23; 
 $this_month = 1089.98; 
 echo $last_month - $this_month; // Prints 97.25
+
+
+$num_cookies = 24;
+$num_friends = 7;
+$cookies_per_friend = $num_cookies / $num_friends;
+echo $cookies_per_friend; // Prints: 3.4285714285714
+
+
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
+echo $days_per_language; //Prints 44
+
+
