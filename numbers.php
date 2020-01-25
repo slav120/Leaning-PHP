a049f5b4817bbb8519d893cfe83f6b81cb369bc1
Slav <slalev57@me.com>
2020-01-25 17:58:06 -0500

    addition & substracting variables

--- numbers.php
+++ numbers.php
@@ -5,3 +5,24 @@ $my_float = -897.21;
 
 echo $my_int; // Prints: 78
 echo $my_float; // Prints: -897.21
+
+
+$tadpole_age = 7;
+$lily_age = 6; 
+$age_difference = $tadpole_age - $lily_age;
+echo $age_difference; // Prints 1
+
+$favorite_num = 22;
+echo $favorite_num + 1; // Prints 23
+echo $favorite_num; //Prints 22
+
+$age = 82;
+echo $age; // Prints: 82
+
+$age = $age + 2;
+echo $age; // Prints: 84
+
+
+$last_month = 1187.23; 
+$this_month = 1089.98; 
+echo $last_month - $this_month; // Prints 97.25
