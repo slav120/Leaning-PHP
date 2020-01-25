847333f50b72072cf3ab043bb32952a537de675c
Slav <slalev57@me.com>
2020-01-25 17:19:51 -0500

    learning how to concatinate

---
+++ concatenating.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$full_name = "Aisle";
+$full_name .= " Nevertell";
+echo $full_name; // Prints: Aisle Nevertell
+
+$full_name = "Aisle" . " " . "Nevertell" . " " . "Nomaderwat";
+echo $full_name; // Prints: Aisle Nevertell Nomaderwat
+
+$full_name = "Aisle";
+$full_name = $full_name . " Nevertell";
+echo $full_name; // Prints: Aisle Nevertell
+
+$full_name = "Aisle";
+$full_name .= " Nevertell";
+echo $full_name; // Prints: Aisle Nevertell
