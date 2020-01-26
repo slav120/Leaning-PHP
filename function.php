21778d35e799aecc116b760c5c772b2b6a646ccf
Slav <slalev57@me.com>
2020-01-26 18:08:01 -0500

    adding to functions

--- function.php
+++ function.php
@@ -69,3 +69,36 @@ function calculateVolume($height, $width, $volume)
 echo calculateVolume(5, 5, 5);  //prints 125
 
 
+// Write your code below:
+function calculateTip($total, $tip = 20)
+{
+  return $total * (1 + $tip/100);
+}
+/*
+// An Alternate Version:
+function calculateTip($total, $tip = 20)
+{
+  return $total + ($total * $tip/100);
+}
+*/
+
+echo calculateTip(100, 25);
+echo "\n";
+echo calculateTip(100); 
+echo "\n";
+echo calculateTip(65, 15); 
+// returns 125 , 120 , 74.75
+
+
+
+function addX ($param)
+{
+  $param = $param . "X";
+  echo $param;
+};
+$word = "Hello";
+addX($word); // Prints: HelloX
+echo $word; // Prints: Hello
+
+
+
