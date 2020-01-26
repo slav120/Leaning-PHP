5aca5a4030eb90b7817a0840d85703bbac4123e4
Slav <slalev57@me.com>
2020-01-26 17:32:01 -0500

    return statement in php

--- function.php
+++ function.php
@@ -9,3 +9,28 @@ function greetLearner()
 }
 
 
+function greetLearner()
+{
+  echo "Hello, Learner!\n";
+  echo "I hope you're enjoying PHP!\n";
+  echo "Love, Codecademy";
+}
+
+greetLearner(); 
+/*
+Prints:
+Hello, Learner!
+I hope you're enjoying PHP!
+Love, Codecademy
+*/
+
+
+function countdown() 
+{
+  echo "4, 3, 2, 1, ";
+  return "blastoff!";
+}
+
+$return_value = countdown(); // Prints: 4, 3, 2, 1, 
+echo $return_value; // Prints: blastoff!
+
