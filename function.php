b9f63641e4920706aa0a225131c30cfdc951ff55
Slav <slalev57@me.com>
2020-01-26 18:36:22 -0500

    global variables and how to use them inside functions

--- function.php
+++ function.php
@@ -102,3 +102,24 @@ echo $word; // Prints: Hello
 
 
 
+$string_one = "you have teeth";
+$string_two = "toads are nice";
+$string_three = "brown is my favorite color";
+
+// Write your code below:
+function convertToQuestion(&$str)
+{
+  $str = "Do you think " . $str . "?\n";
+}
+
+convertToQuestion($string_one);
+convertToQuestion($string_two);
+convertToQuestion($string_three);
+
+echo $string_one;
+echo $string_two;
+echo $string_three;
+// prints        Do you think you have teeth?
+// Do you think toads are nice?
+// Do you think brown is my favorite color?
+
---
+++ globalVariable.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$language = "PHP";
+$topic = "scope";
+
+function generateLessonName($concept)
+{
+  global $language;
+  return $language . ": " . $concept;
+}
+
+echo generateLessonName($topic);
+// prints PHP: scope
