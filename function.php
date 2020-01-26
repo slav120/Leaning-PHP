fe6551bd2ef6de3758d78fc8bea67fafecac7d8c
Slav <slalev57@me.com>
2020-01-26 17:54:27 -0500

    function variables

--- function.php
+++ function.php
@@ -34,3 +34,38 @@ function countdown()
 $return_value = countdown(); // Prints: 4, 3, 2, 1, 
 echo $return_value; // Prints: blastoff!
 
+
+<?php
+// Write your code below:
+function increaseEnthusiasm($str_param)
+{
+  return $str_param . "!";
+}
+
+function repeatThreeTimes($str_param)
+{
+  return $str_param . $str_param . $str_param;
+}  
+
+echo increaseEnthusiasm("oh");
+
+echo repeatThreeTimes("wow");
+
+echo increaseEnthusiasm(repeatThreeTimes("hai"));
+// prints oh!wowwowwowhaihaihai!
+
+<?php
+// Write your code below:
+function calculateArea($height, $width) 
+{
+  return $height * $width; 
+}
+echo calculateArea(5, 5);  //prints 25 
+
+function calculateVolume($height, $width, $volume)
+{
+  return $height * $width * $volume; 
+}
+echo calculateVolume(5, 5, 5);  //prints 125
+
+
