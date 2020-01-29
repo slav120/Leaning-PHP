73ef15d59491fbf4453f65635d68e6c9ed40dfad
Slav <slalev57@me.com>
2020-01-29 10:21:13 -0500

    gettype function

---
+++ assigningVariables.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$movie = "I am Legend";
+// Add your code here:
+$old_favorite = $movie;
+
+
+echo "I'm a fickle person, my favorite movie used to be $movie.";
+  
+// Add a statement here:
+$movie = "Barney the Dinosaur";
+  
+echo "\nBut now my favorite is $movie.";
+  
+// Add a statement below:
+
+echo "$old_favorite";
+
+// PRINTS
+// I'm a fickle person, my favorite movie used to be I am Legend.
+// But now my favorite is Barney the Dinosaur.I am Legend
---
+++ builtInFunctions.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Codecademy;
+
+$first = "Welcome to the magical world of built-in functions.";
+  
+$second = 82137012983; 
+
+//Write your code below:
+echo gettype($first); // string
+echo gettype($second); // integer 
