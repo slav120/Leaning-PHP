165f3c56b7838e6e766022e51bf361e998acb1ef
Slav <slalev57@me.com>
2020-01-29 10:46:41 -0500

    abs (absolute number function) & round function

--- builtInFunctions.php
+++ builtInFunctions.php
@@ -26,3 +26,14 @@ echo str_repeat("hi", 10); //  hihihihihihihihihihi
 $story = "I was like, \"Dude, like just tell me what you like think because like everyone else is like being totally honest, and like I just want to know what you like think.\" So like I don't know...";
 
 echo substr_count($story, "like"); // Prints: 8
+
+
+echo abs(-10.99); // Prints: 10.99
+
+echo abs(127); // Prints: 127
+
+echo round(1.2); // Prints 1
+
+echo round(1.5); //Prints 2
+
+echo round(1298736.821763876); // Prints: 1298737
