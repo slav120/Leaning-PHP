dd3dd637344f01fb6fc3cbb6716fac65af2906bb
Slav <slalev57@me.com>
2020-01-29 10:39:45 -0500

    strrev , strtolower & str_repeat functions

--- builtInFunctions.php
+++ builtInFunctions.php
@@ -12,3 +12,12 @@ echo gettype($second); // integer
  
 echo var_dump($first); // string(51) "Welcome to the magical world of built-in functions."
 echo var_dump($second); // int(82137012983)
+
+
+
+
+echo strrev("Hello, World!"); // !dlroW ,olleH
+
+echo strtolower("HeLLo"); //  hello
+
+echo str_repeat("hi", 10); //  hihihihihihihihihihi 
