c6284c3894672bacfcef0cc1a60337108228bf8c
Slav <slalev57@me.com>
2020-01-29 10:51:28 -0500

    rand function and getrandmax function (random number)

--- builtInFunctions.php
+++ builtInFunctions.php
@@ -37,3 +37,15 @@ echo round(1.2); // Prints 1
 echo round(1.5); //Prints 2
 
 echo round(1298736.821763876); // Prints: 1298737
+
+
+
+echo getrandmax(); // prints a random number 0 and max 
+
+echo rand(); // Prints a number between 0 and $max
+
+echo rand(1, 2); // Prints either 1 or 2
+
+echo rand(5, 10); // Prints a number between 5 and 10 (inclusive!)
+
+echo rand(1, 100); // Prints a number between 1 and 100 (inclusive!)
