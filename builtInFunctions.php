d432a535eb30c69e04b4669e4dd9f88fba70b3a6
Slav <slalev57@me.com>
2020-01-29 10:33:36 -0500

    var_dump function

--- builtInFunctions.php
+++ builtInFunctions.php
@@ -9,3 +9,6 @@ $second = 82137012983;
 //Write your code below:
 echo gettype($first); // string
 echo gettype($second); // integer 
+ 
+echo var_dump($first); // string(51) "Welcome to the magical world of built-in functions."
+echo var_dump($second); // int(82137012983)
