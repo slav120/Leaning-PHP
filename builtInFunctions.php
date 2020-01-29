9d914916965f50433f17ce9e6125e34cb3721b73
Slav <slalev57@me.com>
2020-01-29 10:44:27 -0500

    substr_count

--- builtInFunctions.php
+++ builtInFunctions.php
@@ -21,3 +21,8 @@ echo strrev("Hello, World!"); // !dlroW ,olleH
 echo strtolower("HeLLo"); //  hello
 
 echo str_repeat("hi", 10); //  hihihihihihihihihihi 
+
+
+$story = "I was like, \"Dude, like just tell me what you like think because like everyone else is like being totally honest, and like I just want to know what you like think.\" So like I don't know...";
+
+echo substr_count($story, "like"); // Prints: 8
