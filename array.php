04bca2446fc9b47f077c63c3137b476c89413e90
Slav <slalev57@me.com>
2020-01-31 12:28:30 -0500

    count function for array

--- array.php
+++ array.php
@@ -3,5 +3,6 @@
 $objects = array(1, 2, 4, 6);  //how to create an array
 $nono = array("ldask", "ndjkjkld", 54654, "hdahd") //can put whatever you want 
 
-$first_array = array("hello", 1, "what", 5, "up"); 
 
+$first_array = array("hello", 1, "what", 5, "up"); 
+echo count($first_array); // returns 5 (5 elements in this string)
