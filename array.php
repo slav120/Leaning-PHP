04779416e489471783999e7227081b9d12c8376b
Slav <slalev57@me.com>
2020-01-31 12:27:00 -0500

    the array in php

---
+++ array.php
@@ -0,0 +1,7 @@
+<?php 
+
+$objects = array(1, 2, 4, 6);  //how to create an array
+$nono = array("ldask", "ndjkjkld", 54654, "hdahd") //can put whatever you want 
+
+$first_array = array("hello", 1, "what", 5, "up"); 
+
