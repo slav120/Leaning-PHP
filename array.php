9876cc601f37b43c23b9718635b86de6a82868f1
Slav <slalev57@me.com>
2020-01-31 12:41:40 -0500

    different ways of printing an array, print_r and implode functions

--- array.php
+++ array.php
@@ -6,3 +6,18 @@ $nono = array("ldask", "ndjkjkld", 54654, "hdahd") //can put whatever you want
 
 $first_array = array("hello", 1, "what", 5, "up"); 
 echo count($first_array); // returns 5 (5 elements in this string)
+
+
+
+
+$number_array = ['hello', 'dear', 'nono']; //shorter syntax 
+
+echo $number_array // returns array 
+print_r($number_array) // returns 
+
+                      // [0] => hello
+                      // [1] => dear 
+                      // [2] => nono
+
+      
+echo implode(", ", $number_array);  // returns hello, dear, nono                      
