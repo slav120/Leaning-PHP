43ee4df7061ee7bf1b5ea5e19a35fd2c92b6d681
Slav <slalev57@me.com>
2020-01-31 13:11:02 -0500

    the array pop and push methods

--- array.php
+++ array.php
@@ -1,7 +1,7 @@
 <?php 
 
 $objects = array(1, 2, 4, 6);  //how to create an array
-$nono = array("ldask", "ndjkjkld", 54654, "hdahd") //can put whatever you want 
+$nono = array("ldask", "ndjkjkld", 54654, "hdahd"); //can put whatever you want 
 
 
 $first_array = array("hello", 1, "what", 5, "up"); 
@@ -12,8 +12,8 @@ echo count($first_array); // returns 5 (5 elements in this string)
 
 $number_array = ['hello', 'dear', 'nono']; //shorter syntax 
 
-echo $number_array // returns array 
-print_r($number_array) // returns 
+echo $number_array; // returns array 
+print_r($number_array); // returns 
 
                       // [0] => hello
                       // [1] => dear 
@@ -23,4 +23,13 @@ print_r($number_array) // returns
 echo implode(", ", $number_array);  // returns hello, dear, nono
 
 
-echo $number_array[0] // returns the element at position 0 withc is hello 
+echo $number_array[0]; // returns the element at position 0 withc is hello 
+
+$number_array[0] = 'blablabla'; // the element at position 0 is now blablabla instead of hello
+echo $number_array[0]; // returns blablabla
+
+$number_array[] = 'klm'; // this will just add to our array, so position 3 is added and is now klm 
+
+
+array_pop($number_array); // removes the last element of the array 
+array_push($number_array, 'yoyo', 'nooo', 'blue'); // add these elements at the end of the array
