94141e9543cff9f6506fb186d254ac3d5bac679e
Slav <slalev57@me.com>
2020-02-02 18:10:50 -0500

    associative arrays, adding keys and values to them in arrays

---
+++ associativeArrays.php
@@ -0,0 +1,17 @@
+<?php
+
+// long syntax
+$php_array = array(
+  "language" => "PHP", 
+  "creator" => "Rasmus Lerdorf", 
+  "year_created" => 1995, 
+  "filename_extensions" => [".php", ".phtml", ".php3", ".php4", ".php5", ".php7", ".phps", ".php-s", ".pht", ".phar"]
+);
+
+// short syntax 
+$php_short = [
+  "language" => "PHP", 
+  "creator" => "Rasmus Lerdorf", 
+  "year_created" => 1995, 
+  "filename_extensions" => [".php", ".phtml", ".php3", ".php4", ".php5", ".php7", ".phps", ".php-s", ".pht", ".phar"]
+];
