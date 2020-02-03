f9a30c5b74bb5a9302bb580676369cf03a3e5a13
Slav <slalev57@me.com>
2020-02-02 20:49:56 -0500

    accessing and adding elements to associative arrays

--- associativeArrays.php
+++ associativeArrays.php
@@ -33,3 +33,29 @@ print_r($september_hits); // returns
 //     [Blue Streak] => 19208806
 //     [Double Jeopardy] => 23162542
 // )
+
+
+$my_array = ["panda"=>"very cute", "lizard"=>"cute", "cockroach"=>"not very cute"];
+echo $my_array["panda"]; // Prints: very cute
+
+$my_array["capybara"] = "cutest";
+echo $my_array["capybara"]; // Prints: cutest
+
+
+
+$assignment_one = ["Alex"=> 87, "Kenny"=> 91, "Natalia"=> 91, "Lily"=> 67, "Dan"=> 81, "Kat"=> 77, "Sara" => 65];
+
+$assignment_two = ["Alex"=> 91, "Kenny"=> 99, "Natalia"=> 100, "Lily"=> 61, "Dan"=> 88, "Kat"=> 90];
+
+$assignment_three = ["Alex"=> 78, "Kenny"=> 92, "Natalia"=> 94, "Lily"=> 79, "Dan"=> 73, "Sara" => 61];
+
+$student_name = "Alex";
+// Write your code below:
+
+$assignment_two["Sara"] = 65;  // changing Sara's grade to 65 
+$assignment_three["Kat"] = 97; // changing Kat's grade to 97 
+
+$dans_grades = [$assignment_one["Dan"], $assignment_two["Dan"], $assignment_three["Dan"]];  // creating variable to hold Dan's grades
+
+
+echo $assignment_two[$student_name];  //91 
