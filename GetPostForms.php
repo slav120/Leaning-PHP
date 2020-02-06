1290fa84b63c90bbf3e7370d3c65567e7611ada3
Slav <slalev57@me.com>
2020-02-06 12:54:15 -0500

    POST and GET requests to the html and getting them back and displaying them

---
+++ GetPostForms.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+$_REQUEST:
+<?php print_r($_REQUEST);?>
+<br>
+$_GET:
+<?php print_r($_GET);?>
+<br>
+$_POST:
+<?php print_r($_POST);?>
+<form method="get">
+GET Form: <input type="text" name="get_name">
+<input type="submit" value="Submit GET">
+</form>
+<form method="post">
+POST Form: <input type="text" name="post_name">
+<input type="submit" value="Submit POST">
+</form>
+<a href="index.php">Reset</a>
+</body>
+</html>
+
+<!-- Creates a html file with forms that updates the POST AND GET 
+
+
+$_REQUEST: Array ( )
+$_GET: Array ( )
+$_POST: Array ( )
+GET Form:   ////////    Submit GET 
+ 
+POST Form:  ///////     Submit POST
+ 
+Reset
+
+ -->
