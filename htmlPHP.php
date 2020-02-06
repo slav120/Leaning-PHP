4046e50f2a526db882e31fc972b04b5ae8413deb
Slav <slalev57@me.com>
2020-02-06 11:15:43 -0500

    adding php to html

---
+++ htmlPHP.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<h1>PHP and HTML</h1>
+<p>This is HTML</p>
+<?php echo "<p>This is PHP</p>";?>  
+
+</body>
+</html>
+
+<!-- Will return a html static render 
+
+PHP and HTML
+This is HTML
+
+This is PHP
+
+
+ -->
